aecfabd7b4fbbe2126a8aa5ba133243df04fb7bc
Don Fox <don@fox>
2023-05-30 23:28:25 +0000

    Add streamlit application

---
+++ app/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+from database import query_db
+from visualization import plot
+
+
+def app():
+    st.title("Enter your well criteria")
+    
+    depth = st.number_input("Min depth", 0, 10_000, value=5000, step=50)
+    gradient = st.number_input("Min gradient", 0, 0.3, value=0.01, step=0.001)
+    
+    well_data = query_db(depth, gradient)
+    chart = plot(well_data)
+    
+    st.write(chart)
+    
+    
+if __name__ == "__main__":
+    app()
