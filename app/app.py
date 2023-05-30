6e41ac22acde9e0c7cc200bc2adadfc445524367
Don Fox <don@fox>
2023-05-30 23:57:57 +0000

    Fix: use all floats for min gradient input

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ def app():
     st.title("Enter your well criteria")
     
     depth = st.number_input("Min depth", 0, 10_000, value=5000, step=50)
-    gradient = st.number_input("Min gradient", 0, 0.3, value=0.01, step=0.001)
+    gradient = st.number_input("Min gradient", 0.0, 0.3, value=0.01, step=0.001)
     
     well_data = query_db(depth, gradient)
     chart = plot(well_data)
